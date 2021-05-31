dc526b3764fc26da767c21b5013d514463e84a16
ErionHoti <77027090+ErionHoti@users.noreply.github.com>
2021-05-31 22:19:50 +0200

    Update BinPackingApplication.java

--- BinPacking/src/BinPackingApplication.java
+++ BinPacking/src/BinPackingApplication.java
@@ -6,7 +6,9 @@ public class BinPackingApplication {
 	public static void main (String[] args) {
 		List<Integer> pakot = Arrays.asList(7,6,5,9,10,5,7,3,4,8);
         int Kapaciteti_i_Koshit = 10;
-
+        for(int i=0;i<=pakot.size()-1;i++)
+	{if ( pakot.get(i)<0)
+		throw new RuntimeException("BinPackingApplication error; ELement negativ ne liste");}
         //StoL c = new StoL(pakot, Kapaciteti_i_Koshit);
         //LtoS c = new LtoS(pakot, Kapaciteti_i_Koshit);
         Random c = new Random(pakot, Kapaciteti_i_Koshit);
